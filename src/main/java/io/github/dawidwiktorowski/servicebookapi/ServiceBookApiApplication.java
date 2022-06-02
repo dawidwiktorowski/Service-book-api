95c8fba25f719ece3824325dba8faf69ededf827
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-02 11:13:14 +0200

    Initial commit

---
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/ServiceBookApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.dawidwiktorowski.servicebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/dawidwiktorowski/servicebookapi/ServiceBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dawidwiktorowski.servicebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
