9433e0bd73e951d819db84a5d2fa662131f1d34b
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-02 13:46:38 +0200

    Added car mapper

---
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/car/CarMapper.java
@@ -0,0 +1,13 @@
+package io.github.dawidwiktorowski.servicebookapi.car;
+
+public class CarMapper {
+
+    static CarDto toDto(Car car) {
+        CarDto carDto = new CarDto();
+        carDto.setId(car.getId());
+        carDto.setMark(car.getMark());
+        carDto.setModel(car.getModel());
+        carDto.setRegisterNumber(car.getRegisterNumber());
+        return carDto;
+    }
+}
