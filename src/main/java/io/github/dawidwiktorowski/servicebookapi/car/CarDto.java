a02679202bd764f9205dbf328a94aa49330f6b74
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-02 13:03:17 +0200

    Created carDto

---
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/car/CarDto.java
@@ -0,0 +1,41 @@
+package io.github.dawidwiktorowski.servicebookapi.car;
+
+public class CarDto {
+
+    private Long id;
+    private String mark;
+    private String model;
+    private int registerNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(int registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+}
