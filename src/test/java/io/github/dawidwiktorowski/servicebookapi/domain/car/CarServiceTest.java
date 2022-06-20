783f35f3efd4a77fd136e556225b43f6e051bc73
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-20 16:58:12 +0200

    added find by mark method

--- src/test/java/io/github/dawidwiktorowski/servicebookapi/domain/car/CarServiceTest.java
+++ src/test/java/io/github/dawidwiktorowski/servicebookapi/domain/car/CarServiceTest.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CarServiceTest {
 
     @Mock
     private CarRepository carRepository;
+    @Mock
+    private CarDto carDto;
     private CarService underTest;
 
     @BeforeEach
