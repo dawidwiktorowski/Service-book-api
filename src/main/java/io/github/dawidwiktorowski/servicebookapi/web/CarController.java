a604c17dcf4c1d447383eaf931e6c8a459f479c3
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-20 16:01:37 +0200

    added find all method

--- src/main/java/io/github/dawidwiktorowski/servicebookapi/web/CarController.java
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/web/CarController.java
@@ -1,7 +1,29 @@
 package io.github.dawidwiktorowski.servicebookapi.web;
 
+import io.github.dawidwiktorowski.servicebookapi.domain.car.CarDto;
+import io.github.dawidwiktorowski.servicebookapi.domain.car.CarService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/car")
 public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("")
+    public List<CarDto> findAll(@RequestParam(required = false) String mark){
+        if (mark != null)
+            return carService.findByMark(mark);
+        else
+            return carService.findAll();
+    }
 }
