89d89460468c86d92faa3058847d50ea55a39ed4
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-21 21:24:31 +0200

    Added findByPesel

--- src/main/java/io/github/dawidwiktorowski/servicebookapi/domain/owner/OwnerRepository.java
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/domain/owner/OwnerRepository.java
@@ -3,8 +3,10 @@ package io.github.dawidwiktorowski.servicebookapi.domain.owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByPesel(String pesel);
 
     List<Owner> findAllByLastNameContainingIgnoreCase(String lastName);
 }
