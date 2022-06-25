1c7da792025bb6471e097cc2774409514f196b91
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-25 10:35:13 +0200

    Added assignments list

--- src/main/java/io/github/dawidwiktorowski/servicebookapi/domain/owner/Owner.java
+++ src/main/java/io/github/dawidwiktorowski/servicebookapi/domain/owner/Owner.java
@@ -1,6 +1,10 @@
 package io.github.dawidwiktorowski.servicebookapi.domain.owner;
 
+import io.github.dawidwiktorowski.servicebookapi.domain.assignment.Assignment;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Owner {
@@ -13,6 +17,9 @@ public class Owner {
     private String phoneNumber;
     @Column(unique = true)
     private String pesel;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Assignment> assignments = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -52,5 +59,13 @@ public class Owner {
 
     public void setPesel(String pesel) {
         this.pesel = pesel;
+    }
+
+    public List<Assignment> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(List<Assignment> assignments) {
+        this.assignments = assignments;
     }
 }
