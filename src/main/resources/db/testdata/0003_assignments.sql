1188114274bb5f007c56125d02278916c8f09e74
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-25 10:33:13 +0200

    Created data test

---
+++ src/main/resources/db/testdata/0003_assignments.sql
@@ -0,0 +1,4 @@
+insert into assignment( start, ends, owner_id, car_id) values
+                                                              ('2017-10-08 15:00:00', '2018-10-08 15:00:00', 1, 1),
+                                                              ('2018-10-09 12:00:00', null, 1, 5),
+                                                              ('2018-10-10 16:00:00', null, 1, 9);
